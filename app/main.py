2133acf802999cf288506b9b69e0052639e79395
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-04-12 16:15:15 -0300

    feat: adicionar handler para validação de exception 422

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 
+from app.presentation.exceptions.handlers import validation_exception_handler
 from app.presentation.middleware.tenant_manager import TenantMiddleware
 from app.presentation.routes import router
 
 app = FastAPI()
 
 app.add_middleware(TenantMiddleware)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
 
 app.include_router(router)
