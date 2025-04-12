2d45279967a93e2575c2554e50c9dd78d541c4db
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-04-12 16:07:50 -0300

    feat: implementar funções que retornam status code e mensagem correta para as rotas

---
+++ app/presentation/http/response.py
@@ -0,0 +1,53 @@
+from typing import Any, Union
+
+from fastapi import status
+
+from app.presentation.schemas.response_schema import SchemaResponseHttp
+
+
+def build_response(
+        status_code: int,
+        type_: str,
+        message: str,
+        data: Union[dict, list[dict], str] | None
+) -> SchemaResponseHttp:
+    return SchemaResponseHttp(
+        status_code=status_code,
+        body={
+            "type": type_,
+            "message": message,
+            "data": data
+        }
+    )
+
+
+def response_ok(message: str, data: Any = None) -> SchemaResponseHttp:
+    return build_response(status.HTTP_200_OK, "success", message, data)
+
+
+def response_created(message: str, data: Any = None) -> SchemaResponseHttp:
+    return build_response(status.HTTP_201_CREATED, "success", message, data)
+
+
+def response_no_content(message: str, data: Any = None) -> SchemaResponseHttp:
+    return build_response(status.HTTP_202_ACCEPTED, "success", message, data | None)
+
+
+def response_bad_request(message: str, data: Any = None) -> SchemaResponseHttp:
+    return build_response(status.HTTP_400_BAD_REQUEST, "error", message, data)
+
+
+def response_unprocessable_entity(message: str, data: Any = None) -> SchemaResponseHttp:
+    return build_response(status.HTTP_422_UNPROCESSABLE_ENTITY, "error", message, data)
+
+
+def response_not_found(message: str, data: Any = None) -> SchemaResponseHttp:
+    return build_response(status.HTTP_404_NOT_FOUND, "error", message, data)
+
+
+def response_conflict(message: str, data: Any = None) -> SchemaResponseHttp:
+    return build_response(status.HTTP_409_CONFLICT, "error", message, data)
+
+
+def response_internal_error(message: str, data: Any = None) -> SchemaResponseHttp:
+    return build_response(status.HTTP_500_INTERNAL_SERVER_ERROR, "error", message, data)
