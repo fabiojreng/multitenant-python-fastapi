c27a84f6649e166cbfa58ef3e6daa4aaadb5f61c
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-04-13 21:32:04 -0300

    feat: adicionar atributos e tipos para validação dos schemas de product

--- app/presentation/schemas/product_schema.py
+++ app/presentation/schemas/product_schema.py
@@ -1,24 +1,24 @@
-from datetime import datetime
-
 from pydantic import BaseModel
-
-from app.domain.value_objects.status import ProductStatus
 
 
 class ProductCreateSchema(BaseModel):
     name: str
-    # image: str | None
-    # brand_id: str
-    # category_id: str
-    # supplier_id: str
-    # quantity: int
-    # quantity_min: int | None
-    # price_cost: float
-    # selling_price: float
-    # validate_date: date | None
+    image: str | None
+    brand_id: str
+    category_id: str
+    supplier_id: str
+    quantity: int
+    quantity_min: int | None
+    price_cost: float
+    selling_price: float
+    validate_date: str | None
 
 
 class ProductResponseSchema(ProductCreateSchema):
     product_id: str
-    created_at: datetime
-    status: ProductStatus
+    created_at: str
+    status: str
+
+
+class ProductListResponseSchema(BaseModel):
+    products: list[ProductResponseSchema]
