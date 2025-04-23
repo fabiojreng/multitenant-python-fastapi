13489b574775723a86e693ab9afbf6f24ca48b17
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-04-23 11:08:56 -0300

    feat: adicionar rota de busca de produto por id

--- app/presentation/routes/product_routes.py
+++ app/presentation/routes/product_routes.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Request
 
 from app.application.use_cases.product.create_product_use_case import CreateProductUseCase
-from app.application.use_cases.product.list_product_use_case import ListProductsUseCase
+from app.application.use_cases.product.find_product_id_use_case import FindProductIdUseCase
+from app.application.use_cases.product.list_products_use_case import ListProductsUseCase
 from app.infra.repositories.postgreSQL.product_repository import ProductRepository
 from app.infra.repositories.postgreSQL.stock_movements_repository import StockMovementsRepository
 from app.presentation.helpers.http_response_helper import http_response
@@ -24,3 +25,10 @@ def list_products(request: Request):
 def create_product(request: Request, product_data: ProductCreateSchema):
     use_case = get_use_case(request, CreateProductUseCase, ProductRepository, StockMovementsRepository)
     return http_response(use_case.execute(product_data.model_dump()))
+
+
+@router.get("/product/{product_id}", response_model=SchemaResponseHttp,
+            responses={500: {"model": SchemaResponseHttp}})
+def find_product_by_id(request: Request, product_id: str):
+    use_case = get_use_case(request, FindProductIdUseCase, ProductRepository)
+    return http_response(use_case.execute(product_id))
