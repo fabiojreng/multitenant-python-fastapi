8a07b11692f5a5607c0211ce2fb66742abac7864
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-04-24 13:36:44 -0300

    feat: adicionar decorator para tornar método abstrato

--- app/domain/interfaces/entities/ibrand_repository.py
+++ app/domain/interfaces/entities/ibrand_repository.py
@@ -6,8 +6,10 @@ class BrandRepositoryInterface(ABC):
     def list_all(self) -> list[dict]:
         pass
 
+    @abstractmethod
     def create(self, params: dict) -> list[dict]:
         pass
 
+    @abstractmethod
     def find_by_id(self, brand_id: str) -> dict:
         pass
