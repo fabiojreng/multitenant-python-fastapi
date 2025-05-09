15f5b3b7ff1543ab1f953e064dc7ff141854395c
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-05-09 12:33:32 -0300

    feat: implementar a atualização da quantidade do produto a cada compra

--- app/infra/repositories/postgreSQL/product_repository.py
+++ app/infra/repositories/postgreSQL/product_repository.py
@@ -10,7 +10,21 @@ class ProductRepository(ProductRepositoryInterface):
     def list_all(self) -> list[dict]:
         with self.__db.get_session() as session:
             products_orm = session.query(ProductORM).all()
-            return [product.to_entity() for product in products_orm]
+            products = []
+
+            for product in products_orm:
+                total_quantity = sum(s.quantity for s in product.shoppings)
+                last_purchase = max((s.created_at for s in product.shoppings), default=None)
+
+                product_data = product.to_entity()
+                product_data.update({
+                    "quantity": total_quantity,
+                    "created_at": last_purchase
+                })
+
+                products.append(product_data)
+
+            return products
 
     def create(self, params: dict) -> dict:
         model = ProductORM.from_entity(params)
@@ -19,3 +33,22 @@ class ProductRepository(ProductRepositoryInterface):
             session.commit()
             session.refresh(model)
         return model.to_entity()
+
+    def find_by_id(self, product_id: str) -> dict:
+        with self.__db.get_session() as session:
+            try:
+                product = session.query(ProductORM).filter_by(product_id=product_id).one()
+
+                total_quantity = sum(s.quantity for s in product.shoppings)
+                last_purchase = max((s.created_at for s in product.shoppings), default=None)
+
+                base = product.to_entity()
+                base.update({
+                    "quantity": total_quantity,
+                    "created_at": last_purchase
+                })
+
+                return base
+
+            except:
+                return None
