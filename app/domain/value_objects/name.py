19101ba89bd9c4245307aaf0af05ef1db5010cd3
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-05-09 12:23:42 -0300

    feat: adicionar outras opções para a regex

--- app/domain/value_objects/name.py
+++ app/domain/value_objects/name.py
@@ -3,7 +3,7 @@ import re
 
 class Name:
     def __init__(self, name: str) -> None:
-        if not re.match(r"^[A-Za-zÀ-ÖØ-öø-ÿ\s\'-]+$", name):
+        if not re.match(r"^[A-Za-zÀ-ÖØ-öø-ÿ\s\'\.-]+$", name):
             raise ValueError("Invalid name")
         self._value = name
 
