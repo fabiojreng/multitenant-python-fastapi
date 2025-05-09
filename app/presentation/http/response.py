2df663462b9fc3e7efba7ae2b7633384c5aa7977
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-05-09 12:39:03 -0300

    feat: adicionar tipagem correta para o body

--- app/presentation/http/response.py
+++ app/presentation/http/response.py
@@ -1,8 +1,9 @@
 from typing import Any, Union
 
 from fastapi import status
+from starlette.responses import JSONResponse
 
-from app.presentation.schemas.response_schema import SchemaResponseHttp
+from app.presentation.schemas.response_schema import SchemaResponseHttp, SchemaResponseBody
 
 
 def build_response(
@@ -13,11 +14,11 @@ def build_response(
 ) -> SchemaResponseHttp:
     return SchemaResponseHttp(
         status_code=status_code,
-        body={
-            "type": type_,
-            "message": message,
-            "data": data
-        }
+        body=SchemaResponseBody(
+            type=type_,
+            message=message,
+            data=data
+        )
     )
 
 
@@ -30,11 +31,15 @@ def response_created(message: str, data: Any = None) -> SchemaResponseHttp:
 
 
 def response_no_content(message: str, data: Any = None) -> SchemaResponseHttp:
-    return build_response(status.HTTP_202_ACCEPTED, "success", message, data | None)
+    return build_response(status.HTTP_202_ACCEPTED, "success", message, data)
 
 
 def response_bad_request(message: str, data: Any = None) -> SchemaResponseHttp:
     return build_response(status.HTTP_400_BAD_REQUEST, "error", message, data)
+
+
+def response_conflict(message: str, data: Any = None) -> SchemaResponseHttp:
+    return build_response(status.HTTP_409_CONFLICT, "error", message, data)
 
 
 def response_unprocessable_entity(message: str, data: Any = None) -> SchemaResponseHttp:
@@ -45,9 +50,12 @@ def response_not_found(message: str, data: Any = None) -> SchemaResponseHttp:
     return build_response(status.HTTP_404_NOT_FOUND, "error", message, data)
 
 
-def response_conflict(message: str, data: Any = None) -> SchemaResponseHttp:
-    return build_response(status.HTTP_409_CONFLICT, "error", message, data)
-
-
 def response_internal_error(message: str, data: Any = None) -> SchemaResponseHttp:
     return build_response(status.HTTP_500_INTERNAL_SERVER_ERROR, "error", message, data)
+
+
+def as_json_response(schema_response: SchemaResponseHttp) -> JSONResponse:
+    return JSONResponse(
+        status_code=schema_response.status_code,
+        content=schema_response.body.dict()
+    )
