f5836e6bf75d66ac360a9f8ece4d1f7f52fce26c
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-05-09 12:32:41 -0300

    refactor: retornar a response em formato json enconder

--- app/presentation/helpers/http_response_helper.py
+++ app/presentation/helpers/http_response_helper.py
@@ -1,10 +1,11 @@
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 from app.presentation.schemas.response_schema import SchemaResponseHttp
 
 
-def http_response(response: SchemaResponseHttp) -> JSONResponse:
+def http_response(response: SchemaResponseHttp):
     return JSONResponse(
         status_code=response.status_code,
-        content=response.dict()
+        content=jsonable_encoder(response)
     )
