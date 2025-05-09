8c2715db111cf9920b998261d958fa652b42b449
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-05-09 12:40:21 -0300

    feat: incluir novas rotas da aplicação

--- app/presentation/routes/__init__.py
+++ app/presentation/routes/__init__.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter
 
 from app.presentation.routes.brand_routes import router as brand_router
+from app.presentation.routes.category_routes import router as category_router
 from app.presentation.routes.product_routes import router as product_router
+from app.presentation.routes.shopping_routes import router as shopping_router
+from app.presentation.routes.supplier_routes import router as supplier_router
 
 router = APIRouter()
 
 router.include_router(product_router)
 router.include_router(brand_router)
+router.include_router(category_router)
+router.include_router(supplier_router)
+router.include_router(shopping_router)
 
 # router.include_router(product_router, prefix="/api", tags=["Products"])
 # router.include_router(brand_router, prefix="/api", tags=["Brands"])
