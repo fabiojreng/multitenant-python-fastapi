2164a0b8497fc756c0e09b295198c916f104b170
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-05-09 12:29:59 -0300

    feat: alterar os atributos da entidade

--- app/domain/entities/shopping.py
+++ app/domain/entities/shopping.py
@@ -2,39 +2,62 @@ from datetime import datetime
 from uuid import uuid4
 
 
-class StockMovement:
+class Shopping:
     def __init__(
             self,
-            stock_movement_id: str,
+            shopping_id: str,
+            supplier_id: str,
             product_id: str,
-            quantity_changed: int,
-            movement_type: int,
-            reason: str | None,
-            date_moviment: datetime | None
+            price_cost: float,
+            quantity: int,
+            created_at: datetime
     ):
-        self.__stock_movement_id = stock_movement_id
+        self.__shopping_id = shopping_id
+        self.__supplier_id = supplier_id
         self.__product_id = product_id
-        self.__quantity_changed = quantity_changed
-        self.__movement_type = movement_type
-        self.__reason = reason
-        self.__date_moviment = date_moviment or datetime.now()
+        self.__price_cost = price_cost
+        self.__quantity = quantity
+        self.__created_at = created_at
 
     @staticmethod
-    def create(product_id: str, quantity_changed: int, moviment_type: int, reason: str | None,
-               date_moviment: str | None) -> "StockMovement":
-        return StockMovement(uuid4(), product_id, quantity_changed, moviment_type, reason, date_moviment)
+    def create(supplier_id: str, product_id: str, price_cost: float, quantity: int) -> "Shopping":
+        return Shopping(str(uuid4()), supplier_id, product_id, price_cost, quantity, datetime.utcnow())
 
     @staticmethod
-    def restore(stock_moviment_id: str, product_id: str, quantity_changed: int, moviment_type: int, reason: str | None,
-                date_moviment: str | None) -> "StockMovement":
-        return StockMovement(stock_moviment_id, product_id, quantity_changed, moviment_type, reason, date_moviment)
+    def restore(
+            shopping_id: str,
+            supplier_id: str,
+            product_id: str,
+            price_cost: float,
+            quantity: int,
+            created_at: datetime
+    ) -> "Shopping":
+        return Shopping(shopping_id, supplier_id, product_id, price_cost, quantity, created_at)
 
     def to_dict(self):
         return {
-            "stock_movement_id": str(self.__stock_movement_id),
-            "product_id": str(self.__product_id),
-            "quantity_changed": self.__quantity_changed,
-            "movement_type": self.__movement_type,
-            "reason": self.__reason,
-            "date_moviment": self.__date_moviment.isoformat(),
+            "shopping_id": self.__shopping_id,
+            "supplier_id": self.__supplier_id,
+            "product_id": self.__product_id,
+            "price_cost": self.__price_cost,
+            "quantity": self.__quantity,
+            "created_at": self.__created_at.isoformat()
         }
+
+    def get_shopping_id(self):
+        return self.__shopping_id
+
+    def get_supplier_id(self):
+        return self.__supplier_id
+
+    def get_product_id(self):
+        return self.__product_id
+
+    def get_price_cost(self):
+        return self.__price_cost
+
+    def get_quantity(self):
+        return self.__quantity
+
+    def get_created_at(self):
+        return self.__created_at
