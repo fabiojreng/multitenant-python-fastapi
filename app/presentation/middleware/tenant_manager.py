c18590103ee4f501993f84cb24d9173936af5274
Fábio Júnior <fabiojunioreloi@hotmail.com>
2025-05-09 12:33:57 -0300

    refactor: tratar exceptions e retornos

--- app/presentation/middleware/tenant_manager.py
+++ app/presentation/middleware/tenant_manager.py
@@ -1,8 +1,11 @@
 from fastapi import Request
-from sqlalchemy.exc import ProgrammingError, SQLAlchemyError
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from app.infra.database.connection_postgreSQL import PostgresConnection
+from app.presentation.http.response import as_json_response, response_bad_request, response_not_found, \
+    response_internal_error
 
 EXCLUDED_PATHS = {"/", "/docs", "/redoc", "/openapi.json"}
 
@@ -16,24 +19,33 @@ class TenantMiddleware(BaseHTTPMiddleware):
             return await call_next(request)
 
         if not tenant:
-            raise ValueError("Tenant not provided")
+            return as_json_response(response_bad_request("Tenant não passado no cabeçalho"))
 
         connection = PostgresConnection(request)
 
         try:
+            engine = connection.get_engine()
+            with engine.connect() as conn:
+                schema_exists = conn.execute(
+                    text("SELECT schema_name FROM information_schema.schemata WHERE schema_name = :schema"),
+                    {"schema": tenant}
+                ).first()
+
+                if not schema_exists:
+                    return as_json_response(response_not_found("Tenant inválido ou schema não existe"))
+
             db_session = connection.connect()
             request.state.db = db_session
             print("conectou com o tenant")
 
             response = await call_next(request)
 
-        except ProgrammingError:
-            db_session.rollback()
-            raise ValueError("Invalid tenant or schema does not exist")
 
         except SQLAlchemyError as e:
-            db_session.rollback()
-            raise ValueError(f"Database error: {str(e)}")
+            return as_json_response(response_internal_error("Erro de banco de dados"))
+
+        except:
+            return as_json_response(response_internal_error("Erro interno inesperado"))
 
         finally:
             connection.close()
